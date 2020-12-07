86a199f6239cc064b366ecbaea6751a43370d317
Yari Andres <yari.andres@gmail.com>
2020-12-07 10:38:18 +0100

    adding form

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App);
+
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+
 app.mount('#app');
 
 
